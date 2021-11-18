9b6e22f1735ddba8c47b80a65d1f36a9e0179bd8
unknownrocks619 <unknownrocks619@hotmail.com>
2021-11-18 17:44:41 +0545

    user_detail table column add

--- resources/views/public/inc/support-text.blade.php
+++ resources/views/public/inc/support-text.blade.php
@@ -1,9 +1,30 @@
     @php
     
         // check if there is new question and check if this user have attended the question.
-        $question = \App\Models\Questions::select("id")->get()->pluck('id');
+        $all_question =\App\Models\Questions::select("id")->get();
+        $last_question = \App\Models\Questions::latest()->first();
+        $question = $all_question->pluck('id');
+        $signed_url = URL::temporarySignedRoute(
+														'public.exam.public_examination_start',
+														now()->addMinute(60) ,
+														[encrypt($last_question->question_collections_id)]);
         $user_answer = \App\Models\UserAnswersSubmit::select('sibir_record_id')->whereIn('question_id',$question)->where('user_login_id',auth()->id())->count();
     @endphp
+
+    <div class="card bg-danger">
+        <div class="card-header bg-danger">
+            <h4 class='card-title text-white'>Exam Notice</h4>
+        </div>
+        <div class="card-body text-white border-none" style="border-bottom:none !important">
+            <!-- <p class='text-white'> -->
+                {!! $last_question->question_title !!}
+            <!-- </p> -->
+        </div>
+        <div class="card-footer bg-danger" style="border-top:none !important">
+            <a href="{{ $signed_url }}" class='btn btn-primary'>Submit My Answer</a>
+        </div>
+    </div>
+
     @if($question->count() > $user_answer )
         <div class="card">
             <div class="card-body">
