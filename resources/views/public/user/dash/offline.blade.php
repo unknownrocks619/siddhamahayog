f2cdd6beac0391f5bb255d9b52431ee7601e3902
unknownrocks619 <unknownrocks619@hotmail.com>
2021-11-28 21:30:01 +0545

    offline video and chapter

--- resources/views/public/user/dash/offline.blade.php
+++ resources/views/public/user/dash/offline.blade.php
@@ -1,17 +1,17 @@
 <div class='col-md-8'>
-						<div class='card'>
-							<div class='card-header'>
-								<h4 class='card-title'>
-									Offline Recording
-									<span class='text-right font-weight-bold pt-1' style="font-size:12px;float:right">
-										<a href="{{ route('public.offline.public_get_offline_videos') }}" class='border-bottom text-danger'>View All</a></span>
-								</h4>
-							</div>
-							<div class='card-body'>
-								<p>
-									We have Moved. To Watch Video	
-								<a href="{{ route('public.offline.public_get_offline_videos') }}">Click Here</a>
-							</p>
-							</div>
-						</div>
-					</div>
+	<div class='card'>
+		<div class='card-header'>
+			<h4 class='card-title'>
+				Offline Recording
+				<span class='text-right font-weight-bold pt-1' style="font-size:12px;float:right">
+					<a href="{{ route('public.offline.public_get_offline_videos') }}" class='border-bottom text-danger'>View All</a></span>
+			</h4>
+		</div>
+		<div class='card-body'>
+			<p>
+				We have Moved. To Watch Video	
+			<a href="{{ route('public.offline.public_get_offline_videos') }}" class='text-danger border-bottom'>Click Here</a>
+		</p>
+		</div>
+	</div>
+</div>
--- resources/views/public/user/offline/chapter_view.blade.php
+++ resources/views/public/user/offline/chapter_view.blade.php
@@ -128,7 +128,7 @@
                                 @if($last_video_watch)
                                     <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/{{ $offline_video->youtube_id }}?title=0&byline=0&portrait=0&badge=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
                                     <p>
-                                        <a href="#">{{ $offline_video->video_title }} | </a> on <a href="#">Siddhamahayog.org</a>.</p>
+                                       Chapter: {{$offline_video->chapter->chapter_name}} | <a href="#">{{ $offline_video->video_title }} | </a> on <a href="#">Siddhamahayog.org</a>.</p>
                                 @else
                                     <h5>Please Select Chapter from left navigation</h5>
                                 @endif
