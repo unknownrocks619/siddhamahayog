8c3eed56c3e2f0907e88f1ab663a48de91aa207d
unknownrocks619 <unknownrocks619@hotmail.com>
2021-12-18 11:02:37 +0545

    test

--- resources/views/admin/users/list.blade.php
+++ resources/views/admin/users/list.blade.php
@@ -65,14 +65,14 @@
 
 @section('page_js')
     <!-- BEGIN: Page Vendor JS-->
-        <script src="{{  ('http://stagg.siddhamahayog.org/admin/app-assets/vendors/js/tables/datatable/jquery.dataTables.min.js') }}"></script>
-        <script src="{{  ('http://stagg.siddhamahayog.org/admin/app-assets/vendors/js/tables/datatable/dataTables.bootstrap4.min.js') }}"></script>
-        <script src="{{  ('http://stagg.siddhamahayog.org/admin/app-assets/vendors/js/tables/datatable/dataTables.buttons.min.js') }}"></script>
-        <script src="{{  ('http://stagg.siddhamahayog.org/admin/app-assets/vendors/js/tables/datatable/buttons.html5.min.js') }}"></script>
-        <script src="{{  ('http://stagg.siddhamahayog.org/admin/app-assets/vendors/js/tables/datatable/buttons.print.min.js') }}"></script>
-        <script src="{{  ('http://stagg.siddhamahayog.org/admin/app-assets/vendors/js/tables/datatable/buttons.bootstrap4.min.js') }}"></script>
-        <script src="{{  ('http://stagg.siddhamahayog.org/admin/app-assets/vendors/js/tables/datatable/pdfmake.min.js') }}"></script>
-        <script src="{{  ('http://stagg.siddhamahayog.org/admin/app-assets/vendors/js/tables/datatable/vfs_fonts.js') }}"></script>
+        <script src="{{ asset ('admin/app-assets/vendors/js/tables/datatable/jquery.dataTables.min.js') }}"></script>
+        <script src="{{ asset ('admin/app-assets/vendors/js/tables/datatable/dataTables.bootstrap4.min.js') }}"></script>
+        <script src="{{ asset ('admin/app-assets/vendors/js/tables/datatable/dataTables.buttons.min.js') }}"></script>
+        <script src="{{ asset ('admin/app-assets/vendors/js/tables/datatable/buttons.html5.min.js') }}"></script>
+        <script src="{{ asset ('admin/app-assets/vendors/js/tables/datatable/buttons.print.min.js') }}"></script>
+        <script src="{{ asset ('admin/app-assets/vendors/js/tables/datatable/buttons.bootstrap4.min.js') }}"></script>
+        <script src="{{ asset ('admin/app-assets/vendors/js/tables/datatable/pdfmake.min.js') }}"></script>
+        <script src="{{ asset ('admin/app-assets/vendors/js/tables/datatable/vfs_fonts.js') }}"></script>
         <!-- END: Page Vendor JS-->
         <script type="text/javascript">
             $(document).ready(function() {
