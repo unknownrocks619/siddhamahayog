2ec250049364fe4b19b8ed2f9f3ae9bcdbcbe2bb
unknownrocks619 <unknownrocks619@hotmail.com>
2021-12-18 17:40:52 +0545

    user list category

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -118,7 +118,7 @@ class UserController extends Controller
             //                 ->make(true);
             //     return $datatable;
             // }
-                $users_table = userDetail::with(["country_name"])->paginate(100);
+                $users = userDetail::with(["country_name"])->paginate(100);
 
             return view('admin.users.list_org',compact("users"));
         }
