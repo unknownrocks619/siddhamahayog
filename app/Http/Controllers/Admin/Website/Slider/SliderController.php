a2358124f7e85b7737fed29dab340a54e396888c
unknownrocks619 <unknownrocks619@hotmail.com>
2022-09-10 15:43:53 +0545

    Slider Background Image Loading bug

--- app/Http/Controllers/Admin/Website/Slider/SliderController.php
+++ app/Http/Controllers/Admin/Website/Slider/SliderController.php
@@ -13,57 +13,62 @@ class SliderController extends Controller
 {
     //
 
-    public function index() {
+    public function index()
+    {
         $sliders = Slider::latest()->paginate(10);
-        return vieW('admin.website.slider.index',compact('sliders'));
+        return vieW('admin.website.slider.index', compact('sliders'));
     }
 
-    public function create() {
+    public function create()
+    {
         return view('admin.website.slider.add');
     }
 
-    public function store(SliderRequest $request) {
+    public function store(SliderRequest $request)
+    {
         $slider = new Slider;
         $slider->title = $request->slider_title;
         $slider->description = $request->image_description;
         $slider->tagline = $request->tagline;
         $slider->display_order = $slider->count() + 1;
         $slider->status = ($request->active) ? true : false;
-        
+
         $plugins  = [
             "donation" => $request->donation_button,
             "services" =>  $request->join_today,
             "join_now" => $request->services_button
         ];
         $slider->plugins = json_encode($plugins);
-        $slider->slider_file = asset (Storage::putFile("public/slider/", $request->file('slider_image')->path()));
+        $slider->slider_file = asset(Storage::putFile("slider/", $request->file('slider_image')->path()));
 
         try {
             $slider->save();
         } catch (\Throwable $th) {
             //throw $th;
             dd($th->getMessage());
-            $request->session()->flash("error","Unable to upload Slider File. Please Try again.");
+            $request->session()->flash("error", "Unable to upload Slider File. Please Try again.");
             return back()->withInput();
         }
-        Cache::put("slider_settings",Slider::get());
+        Cache::put("slider_settings", Slider::get());
 
-        $request->session()->flash("success" , "New Slider image Uploaded.");
-        return redirect()->route('admin.website.slider.admin_slider_edit',$slider->id);
+        $request->session()->flash("success", "New Slider image Uploaded.");
+        return redirect()->route('admin.website.slider.admin_slider_edit', $slider->id);
     }
 
-    public function edit(Slider $slider) {
-        return view('admin.website.slider.edit',compact('slider'));
+    public function edit(Slider $slider)
+    {
+        return view('admin.website.slider.edit', compact('slider'));
     }
 
-    public function update(SliderRequest $request , Slider $slider) {
+    public function update(SliderRequest $request, Slider $slider)
+    {
 
         $slider->title = $request->slider_title;
         $slider->description = $request->image_description;
         $slider->tagline = $request->tagline;
         $slider->display_order = $slider->count() + 1;
         $slider->status = ($request->active) ? true : false;
-        
+
         $plugins  = [
             "donation" => $request->donation_button,
             "services" =>  $request->join_today,
@@ -75,28 +80,28 @@ class SliderController extends Controller
             $slider->save();
         } catch (\Throwable $th) {
             //throw $th;
-            $request->session()->flash('error',"Unable to update Slider Information");
+            $request->session()->flash('error', "Unable to update Slider Information");
             return back()->withInput();
         }
-        Cache::put("slider_settings",Slider::get());
+        Cache::put("slider_settings", Slider::get());
         $request->session()->flash('success', "Slider Image Information updated.");
         return back();
     }
 
-    public function delete(Request $request, Slider $slider) {
+    public function delete(Request $request, Slider $slider)
+    {
 
         try {
             $slider->delete();
         } catch (\Throwable $th) {
             //throw $th;
-            $request->session()->flash('error',"Unable to remove slider.");
+            $request->session()->flash('error', "Unable to remove slider.");
             return back();
         }
 
-        Cache::put("slider_settings",Slider::get());
+        Cache::put("slider_settings", Slider::get());
 
-        $request->session()->flash('success',"Slider information removed.");
+        $request->session()->flash('success', "Slider information removed.");
         return redirect()->route('admin.website.slider.admin_slider_index');
-
     }
 }
