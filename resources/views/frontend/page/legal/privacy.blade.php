18291f07a17d03eb24d407c54fe782b6d661b58d
unknownrocks619 <unknownrocks619@hotmail.com>
2022-09-10 22:12:44 +0545

    Privacy

---
+++ resources/views/frontend/page/legal/privacy.blade.php
@@ -0,0 +1,108 @@
+@extends("frontend.theme.master")
+@section("content")
+<x-banner style="background-image: url({{ asset('assets/images/menu/banners/atirudri-detail-banner.jpg') }})">
+</x-banner>
+
+
+<!-- partial -->
+<div class="section section-padding" style="padding-top:50px">
+    <div class="container">
+        <div class="row ">
+            <div class="col-12 mb-5">
+                <div class="section-title mb-0 text-start">
+                    <h4 class="title">Terms & Conditions</h4>
+                    <div>
+                        Computer Acceptable Use Policy
+
+                        For questions about this Policy, contact:
+                        Siddha Mahayog Spiritual Academy, (e-mail:(jagatguru@siddhamahayog.org)
+
+                        <h4>Overview</h4>
+                        <p>
+                            The purpose of this Policy is to establish acceptable and unacceptable use of electronic devices, network resources, e-mail communications, computer applications, and other electronic information systems at Mahayogi Siddhababa Spiritual Academy (hereinafter referred to as “the Academy”), in conjunction with the established culture of the ethical and lawful practices, openness, trust, and integrity.
+                            The Academy provides computer devices, networks, e-mails, applications, and other electronic information systems, to meet mission, goals, and initiatives and must manage them responsibly to maintain the confidentiality, integrity, and availability of its information assets.
+                            Senior management of the Academy in Nepal, which is ultimately responsible for ensuring adherence to this Policy and any breach thereof, will be heard in the court of Nepal, unless relevant countries’ laws and regulations have exceptions as regards such being heard in the court of Nepal.
+                            Agreeing to this Policy, means that you accept our current policies: “Copyright Information and Privacy Policy” and “Terms and Conditions,” which are published on our official website: siddhamahayog.org.
+                            This Policy requires all users of information assets to comply with the Academy’ policies and protects the Academy against damaging legal and regulatory issues.
+                        </p>
+                        <h4>Scope</h4>
+                        <p>All members of staff, contractors, consultants, and other workers at the Academy, including all personnel affiliated with third parties, must adhere to this Policy. This Policy applies to information assets owned or leased by the Academy, or to devices that connect to the Academy’s network, e-mails, applications, or other electronic information systems at the Academy or at remote locations.</p>
+                        <h4>Policy Statement</h4>
+                        <ul>
+                            <li>You are responsible for exercising good judgement regarding the appropriate use of the Academy’s resources by following the Academy’s policies, standards, and guidelines.</li>
+                            <li>The Academy resources must not be used for any unlawful or prohibited purpose.</li>
+                            <li>If a virus is detected, the Information Technology (IT) team must be informed immediately. The infected system must not be used until permission from the IT team is given to do so.</li>
+                            <li>For security, compliance, and maintenance purposes, authorised IT personnel may monitor and audit equipment, systems and network traffic. Devices found to be interfering with other devices or users on the Academy network may be disconnected.</li>
+                            <li>You are responsible for utilising internet resources responsibly and e-mails lawfully.</li>
+                        </ul>
+                        <h4>Internet</h4>
+                        <ul>
+                            <li>No member of staff of the Academy is allowed to access any illegal content on the Academy internet connection.</li>
+                            <li>No member of staff is allowed to access potentially “bad” sites. This includes being aware of sites that may contain malicious content(s).</li>
+                            <li>No copyrighted material without the required license is allowed to be downloaded</li>
+                            <li>Attempting to install potentially dangerous content is strictly prohibited.</li>
+                            <li>All members of staff are encouraged to be security-aware whilst accessing the Internet from the Academy’s computers.</li>
+                            <li>All members of staff shall exercise caution when connecting to a public network from their devices. This includes only connecting to trusted networks; ensuring their screens are not subject to casual observance; computer devices will not be left unattended at any time.</li>
+                            No applications may be privately installed for the purposes of illegal file-sharing (Torrents, TOR and Usenet, for example), or for the purpose of streaming content from the Internet.
+                            Malicious content(s), including obscene or threatening language, obscene imagery or videography, pornography or content(s) pertaining to gambling, must not be viewed on devices used for work purposes. Likewise, e-mails must not contain such content or hyperlinks to similar.
+                            Any member of staff found to be misusing the Academy’s Internet and e-mail systems in any way, may be subjected to the Academy’s disciplinary procedure, which, after investigations, could result in disciplinary action up to and including summary dismissal for gross misconduct.
+                            The viewing, transmission, downloading, uploading or accessing in any way of any of the following material using the Academy’s communications equipment and systems may also result in disciplinary action up to and including summary dismissal for gross misconduct:
+                            Material which is pornographic, sexist, racist, homophobic, paedophilic or any other discriminatory or otherwise offensive material;
+                            Illegal or criminal material, including material which breaches copyright or any other legally recognised intellectual property right;
+                            Any material which has the object or effect of causing harassment to the recipient;
+                            Material which the user knows, or ought to know is a confidential or restricted information and which they are not authorised to possess;
+                            Any website to which the Academy has blocked access;
+                            The above list is not exhaustive.
+                        </ul>
+                        <h4>System Accounts</h4>
+                        You are responsible for the security of data, accounts and systems under your control.
+                        Passwords must be kept secure and not shared with anyone, including other personnel of the Academy, family members or friends. The password policy must be maintained at all times.
+                        Providing unauthorised access to another individual, either deliberately or through failure to secure access to the Academy’s system, is a violation of this Policy.
+                        You must ensure through legal or technical means that proprietary information remains within the control of the Academy at all times.
+                        The storage of proprietary information on personal device or outside the control of the Academy environments, including devices maintained by a third party with whom the Academy does not have a contractual agreement, is prohibited. This specifically prohibits the use of an e-mail account that is not provided by the Academy, or its customer and partners, for the Academy’s business.
+                        <h4>Computing Assets</h4>
+                        You are responsible for ensuring the protection of assigned Academy assets both in transit and in use. This includes the use of secure carry bags, cases, and other security devices.
+                        All computing devices must be password protected, and when left unattended, the screen must be locked or logged off.
+                        Use of the Academy-owned computer software or applications must be operated in accordance with the Academy’s policies, procedures, and guidelines.
+                        <h4>Network Use</h4>
+                        You are responsible for the security and appropriate use of the Academy’s network resources under your control. Using the Academy’s resources for the following are strictly prohibited:
+                        Causing a security breach to either the Academy or other network resources, including, but not limited to - accessing data, servers, or accounts to which you are authorised; circumventing user authentication on any device; or sniffing network traffic;
+                        Causing a disruption of service to either the Academy’s or other network resources, including, but not limited to, packet spoofing, denial of service, heap or buffer overflows, and forged routing information for malicious purposes;
+                        Introducing honeypots, honeynets, or similar technology on the network;
+                        Violating copyright law, including, but not limited to, illegally duplicating or transmitting copyrighted pictures, music, video, and software; exporting or importing software, technical information, encryption software, or technology in violation of international or regional export control laws;
+                        Use of the Internet or the Academy’s network that violates the Academy’s policies, or local laws.
+                        Intentionally introducing malicious code, including but not limited to, viruses, worms, trojan horses, e-mail bombs, spyware, adware, and keyloggers.
+                        Port scanning or security scanning on a production network unless authorised in advance by the IT team.
+                        <h4>Electronic Communications</h4>
+                        The Internet and e-mail systems are essential tools for members of staff of the Academy. The following are some unacceptable and prohibited use of the Academy’s Internet:
+                        You must not under any circumstances, use Internet facilities to access, download, send, receive or view any materials that will offend any person or the Academy.
+                        Individuals must not download non-work related information from the Internet. To reduce the likelihood of a virus infection, individuals must take care to ensure that the files they download are from trustworthy sources.
+                        You must not view , distribute or store sexually explicit content, images, texts or materials that might be considered indecent, pornographic, obscene, discriminatory, offensive, abusive, sexist, racist or illegal.
+                        Users must not access, or attempt to access any computer material or system for which access authorisation has not been given.
+                        You must not send chain e-mail or spam via e-mail, text messages, pages, instant messages, voice mail, or other forms of electronic communication to any individuals.
+                        You must not Forge, misrepresent, obscure, suppress, or replace a user identity on any electronic communication to mislead the recipient about the sender.
+                        Users must not deliberately introduce, or risk introducing any virus or other harmful software, nor take any action to circumvent any anti-virus precautions established by the Academy.
+                        You must not use the Academy’s e-mail or IP address to engage in conduct that violates the Academy policies or guidelines. You must exercise good judgment to avoid misrepresenting or exceeding your authority in representing the interest of the Academy.
+                        <h4>E-mails</h4>
+                        The Academy provides an e-mail facility as vital link with its customers, suppliers and partners. Its use must be limited to official business only; however, incidental and occasional personal use of e-mail is permitted by the Academy with the understanding that personal messages will be treated the same way as business messages.
+                        The Academy reserves the right to purge identified personal e-mail(s) to preserve the integrity of the e-mail system.
+                        The usage of personal e-mail address for any official correspondence, e.g. sending/receiving, subscription/registration for official feeds/notifications/alerts etc., is strictly prohibited.
+                        No members of staff or contractors shall use the Academy’s e-mail system in any way that may be interpreted as insulting, disruptive or offensive by any other person, or the Academy or which may be harmful to the Academy’s reputation. This includes forwarding any received e-mail.
+                        Enforcement and Certification
+                        If any person found to have violated this Policy shall be subjected to disciplinary action, up to and including termination of person’s service. A violation of this Policy by a temporary worker, contractor or vendor may result in the termination of person’s contract or assignment with the Academy.
+                        I certify that unauthorised access, inspection, use, storage, disclosure, or modification of confidential, sensitive, non-public, or personal information, including my own, or any attempt to engage in, or failure to prevent such acts is prohibited and may result in disciplinary and/ or legal action taken against me.
+                        I, understood and will abide by the Academy ‘s Acceptable Use Policy.
+                        I recognise that these systems are the property of the Academy and that the Academy reserves the right to monitor their usage for any reason at any time. My signature on this document means I have consented to this Policy.
+                        I understand that should I become aware of any misuse of the Academy’s systems, I am obligated to inform management of the Academy of such misuse immediately.
+                        I understand that failure to abide by any of the provisions of this Policy may result in disciplinary against me up to and including immediate termination of my engagement, without prior warning or notice.
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+@endsection
