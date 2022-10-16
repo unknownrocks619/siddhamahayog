3e9730023f088a7a385485aca9bc6f3fdfb75569
unknownrocks619 <unknownrocks619@hotmail.com>
2022-10-16 15:20:29 +0100

    ux changes

---
+++ app/Http/Controllers/Frontend/User/SadhakPortalController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Frontend\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SadhakPortalController extends Controller
+{
+    //
+
+    public function user_detail()
+    {
+    }
+}
---
+++ app/Models/Sadhak/SadhakMember.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Sadhak;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SadhakMember extends Model
+{
+    use HasFactory;
+}
