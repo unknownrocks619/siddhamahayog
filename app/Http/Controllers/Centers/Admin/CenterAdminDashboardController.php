47f321ec02ce58f6d227d9fac49447d4eb8d181c
unknownrocks619 <unknownrocks619@hotmail.com>
2022-12-12 21:26:09 +0000

    redirect

--- app/Http/Controllers/Centers/Admin/CenterAdminDashboardController.php
+++ app/Http/Controllers/Centers/Admin/CenterAdminDashboardController.php
@@ -35,7 +35,7 @@ class CenterAdminDashboardController extends Controller
     {
         if ($member->country == 153) {
             session()->flash('error', 'Member Not found.');
-            return back();
+            return redirect()->route('center.admin.member.show');
         }
         return view('centers.admin.members.edit', compact('member'));
     }
