48952f065aa650bad8255d18ebe1ec244f07090d
unknownrocks619 <unknownrocks619@hotmail.com>
2023-01-17 10:59:36 +0000

    modal popup

--- resources/views/frontend/user/dashboard/buttons/admin.blade.php
+++ resources/views/frontend/user/dashboard/buttons/admin.blade.php
@@ -1,6 +1,6 @@
 @if( ! $program->live)
     <a class="btn btn-danger" href="{{route('admin.program.store_live',[$program->getKey()])}}" data-bs-toggle='modal' data-bs-target='#goLive'>
-        Go Live
+        Go Live {{ $program->program_name}} - {{$program->getKey()}} [{{ $program->id}}]
     </a>
 <x-modal modal="goLive">
     <div id="modal_content">
