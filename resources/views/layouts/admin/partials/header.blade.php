80867a135d46bf5c14b3ab89d2378ba285130dca
binodgiri <unknownrocks619@hotmail.com>
2024-01-26 18:05:39 +0000

    navigation things

--- resources/views/layouts/admin/partials/header.blade.php
+++ resources/views/layouts/admin/partials/header.blade.php
@@ -1,6 +1,11 @@
 <!-- Navbar -->
 <nav class="layout-navbar navbar navbar-expand-xl align-items-center bg-navbar-theme" id="layout-navbar">
     <div class="container-xxl">
+        <div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0  d-xl-none ">
+            <a class="nav-item nav-link px-0 me-xl-4" href="javascript:void(0)">
+                <i class="ti ti-menu-2 ti-sm"></i>
+            </a>
+        </div>
         <div class="navbar-brand app-brand demo d-none d-xl-flex py-0 me-4">
             <a href="{{route('admin.admin_dashboard')}}" class="app-brand-link gap-2">
                         <span class="app-brand-logo demo">
@@ -15,11 +20,6 @@
             </a>
             <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-xl-none">
                 <i class="ti ti-x ti-sm align-middle"></i>
-            </a>
-        </div>
-        <div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0  d-xl-none ">
-            <a class="nav-item nav-link px-0 me-xl-4" href="javascript:void(0)">
-                <i class="ti ti-menu-2 ti-sm"></i>
             </a>
         </div>
         <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
